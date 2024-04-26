38bab852d3f9c2a27953b202d73000174bf3868a
Michał Kochanowicz <michal.kochanowicz@student.pk.edu.pl>
2024-04-26 19:01:39 +0200

    endpoints

--- src/ActivitiesService/Controllers/ActivitiesController.cs
+++ src/ActivitiesService/Controllers/ActivitiesController.cs
@@ -1,4 +1,6 @@
 using ActivitiesService.Data;
+using ActivitiesService.DTOs;
+using ActivitiesService.Entities;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -43,5 +45,72 @@ public class ActivitiesController : ControllerBase
         return _mapper.Map<ActivityDto>(activity);
     }
     
-    
+    [HttpPost]
+    public async Task<ActionResult<ActivityDto>> CreateActivity(CreateActivityDto activityDto)
+    {
+        var activity = _mapper.Map<Activity>(activityDto);
+
+        activity.Author = "test";
+
+        _context.Activities.Add(activity);
+
+        var result = await _context.SaveChangesAsync() > 0;
+
+        if (!result)
+        {
+            return BadRequest("Could not save changes to the database");
+        }
+
+        return CreatedAtAction(nameof(GetActivityById), new {activity.Id }, _mapper.Map<ActivityDto>(activity));
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult> UpdateActivity(Guid id, UpdateActivityDto updateActivityDto)
+    {
+        var activity = await _context.Activities.Include(x => x.Task).FirstOrDefaultAsync(x => x.Id == id);
+
+        if (activity == null)
+        {
+            return NotFound();
+        }
+
+        activity.Task.Name = updateActivityDto.Name ?? activity.Task.Name;
+        activity.Task.Description = updateActivityDto.Description ?? activity.Task.Description;
+        activity.Task.PredictedTime = updateActivityDto.PredictedTime ?? activity.Task.PredictedTime;
+        activity.Task.Category = updateActivityDto.Category ?? activity.Task.Category;
+        activity.Task.Experience = updateActivityDto.Experience ?? activity.Task.Experience;
+
+        var result = await _context.SaveChangesAsync() > 0;
+
+        if (!result)
+        {
+            return BadRequest("Could not save changes");
+        }
+
+        return Ok(); 
+
+    }
+
+    [HttpDelete("{id}")]    
+    public async Task<ActionResult> DeleteActivity(Guid id)
+    {
+        var activity = await _context.Activities.FindAsync(id);
+
+        if (activity == null)
+        {
+            return NotFound();
+        }
+
+        _context.Activities.Remove(activity);
+
+        var result = await _context.SaveChangesAsync() > 0;
+
+        if (!result)
+        {
+            return BadRequest("Could not update DB");
+        }
+
+        return Ok();
+    }
+
 }
--- src/ActivitiesService/DTOs/UpdateActivityDto.cs
+++ src/ActivitiesService/DTOs/UpdateActivityDto.cs
@@ -6,6 +6,6 @@ public class UpdateActivityDto
         public string Description {get; set;}
         public int? PredictedTime {get; set;}
         public string Category {get; set;}
-        public int Experience {get; set;}
+        public int? Experience {get; set;}
 
 }
