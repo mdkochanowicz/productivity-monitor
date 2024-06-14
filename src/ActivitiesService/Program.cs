6cf1a7cc9d61b7d93c3423ce81db02aeba9ba049
Michał Kochanowicz <michal.kochanowicz@student.pk.edu.pl>
2024-06-14 20:13:41 +0200

    rabbitMQ-start

--- src/ActivitiesService/Program.cs
+++ src/ActivitiesService/Program.cs
@@ -1,4 +1,6 @@
+using System.Security.Cryptography;
 using ActivitiesService.Data;
+using MassTransit;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +13,13 @@ builder.Services.AddDbContext<ActivityDBContext>(opt =>
 });
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddMassTransit(x =>
+{
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.ConfigureEndpoints(context);
+    });
+});
 
 var app = builder.Build();
 
--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,4 +1,5 @@
 using System.Net;
+using MassTransit;
 using MongoDB.Driver;
 using MongoDB.Entities;
 using Polly;
@@ -12,6 +13,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddHttpClient<ActivitySvcHttpClient>().AddPolicyHandler(GetPolicy());
+builder.Services.AddMassTransit(x =>
+{
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.ConfigureEndpoints(context);
+    });
+});
 
 var app = builder.Build();
 
