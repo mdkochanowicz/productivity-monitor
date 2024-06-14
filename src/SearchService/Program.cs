4a9bad1449c913c164595522b606d7e58632bd85
Michał Kochanowicz <michal.kochanowicz@student.pk.edu.pl>
2024-06-14 12:25:51 +0200

    Task->Item

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,3 +1,9 @@
+using System.Net;
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService;
+//using MongoDB.Entities.Core; // Add this line
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -11,5 +17,13 @@ var app = builder.Build();
 app.UseAuthorization();
 
 app.MapControllers();
+
+await DB.InitAsync("searchDb", MongoClientSettings.FromConnectionString(builder.Configuration.GetConnectionString("MongoDbConnection")));
+
+await DB.Index<Item>()
+    .Key(t => t.Name, KeyType.Text)
+    .Key(t => t.Description, KeyType.Text)
+    .Key(t => t.Category, KeyType.Text)
+    .CreateAsync();
 
 app.Run();
